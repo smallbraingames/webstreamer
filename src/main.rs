4d2f4dfcbb55f6efe05c4710ef99a47d237c5979
smallbraingames <smallbraindev@gmail.com>
2025-04-08 20:33:02 -0700

    feat: use env vars regularly

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod browser_capture;
-mod env;
 mod twitch;
 mod ws;
 use browser_capture::CapturedBrowser;
 use futures::SinkExt;
-use std::time::Duration;
+use std::{env, time::Duration};
 use tokio::{join, spawn, sync::mpsc, time::sleep};
 use tokio_tungstenite::tungstenite::{Bytes, Message};
 use tracing::Level;
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
 use twitch::run_twitch;
-
 use ws::run_ws_stream;
 
 const WS_PORT: u16 = 8080;
@@ -25,7 +23,11 @@ async fn main() {
     let (stream_tx, stream_rx) = mpsc::channel::<Bytes>(10);
     let (ws_json_tx, mut ws_json_rx) = mpsc::channel::<String>(10);
 
-    let (twitch_server_handle, twitch_event_handle) = run_twitch(stream_rx, ws_json_tx).await;
+    let twitch_client_id = env::var("TWITCH_CLIENT_ID").unwrap();
+    let twitch_rmtp_url = env::var("TWITCH_RMTP_URL").unwrap();
+
+    let (twitch_server_handle, twitch_event_handle) =
+        run_twitch(&twitch_client_id, &twitch_rmtp_url, stream_rx, ws_json_tx).await;
 
     let mut captured_browser = CapturedBrowser::new((1280, 720)).await;
     let browser_handle = spawn(async move {
